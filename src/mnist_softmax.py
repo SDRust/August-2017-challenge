f02f81485e849d07ffea7858332ed9e8db4871ef
Jason Knight <jason@nervanasys.com>
2017-08-06 14:13:23 -0700

    pep8 formatting

--- src/mnist_softmax.py
+++ src/mnist_softmax.py
@@ -38,71 +38,72 @@ FLAGS = None
 
 
 def main(_):
-  # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    # Import data
+    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
-  # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
-  W = tf.Variable(tf.zeros([784, 10]))
-  b = tf.Variable(tf.zeros([10]))
-  y = tf.matmul(x, W) + b
-  prediction = tf.nn.softmax(y)
+    # Create the model
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.matmul(x, W) + b
+    prediction = tf.nn.softmax(y)
 
-  # Ground truth labels
-  y_ = tf.placeholder(tf.float32, [None, 10])
+    # Ground truth labels
+    y_ = tf.placeholder(tf.float32, [None, 10])
 
-  # Define loss and optimizer
-  # The raw formulation of cross-entropy,
-  #
-  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-  #                                 reduction_indices=[1]))
-  #
-  # can be numerically unstable.
-  #
-  # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-  # outputs of 'y', and then average across the batch.
-  cross_entropy = tf.reduce_mean(
+    # Define loss and optimizer
+    # The raw formulation of cross-entropy,
+    #
+    #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+    #                                 reduction_indices=[1]))
+    #
+    # can be numerically unstable.
+    #
+    # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
+    # outputs of 'y', and then average across the batch.
+    cross_entropy = tf.reduce_mean(
       tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy,
-          name='train')
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy,
+                                                                 name='train')
 
-  sess = tf.InteractiveSession()
+    sess = tf.InteractiveSession()
 
-  builder = SavedModelBuilder('saved_model')
+    builder = SavedModelBuilder('saved_model')
 
-  tf.global_variables_initializer().run()
-  # Train
-  for _ in range(2000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    tf.global_variables_initializer().run()
+    # Train
+    for _ in range(2000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-  # Test trained model
-  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+    # Test trained model
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                        y_: mnist.test.labels}))
 
-  signature_inputs = {
+    signature_inputs = {
       "x": build_tensor_info(x),
       "y": build_tensor_info(y_)
-  }
-  signature_outputs = {
+    }
+    signature_outputs = {
       "out": build_tensor_info(prediction)
-  }
-  signature_def = build_signature_def(
+    }
+    signature_def = build_signature_def(
       signature_inputs, signature_outputs,
       REGRESS_METHOD_NAME)
-  builder.add_meta_graph_and_variables(
+    builder.add_meta_graph_and_variables(
       sess, [TRAINING, SERVING],
       signature_def_map={
           REGRESS_METHOD_NAME: signature_def
       },
       assets_collection=tf.get_collection(tf.GraphKeys.ASSET_FILEPATHS))
-  builder.save(as_text=False)
+    builder.save(as_text=False)
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str,
+                        default='/tmp/tensorflow/mnist/input_data',
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
